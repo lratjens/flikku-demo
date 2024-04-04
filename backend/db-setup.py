8d19b443dea762d6314c1141fa6b1ac7288694dd
Lachlan Ratjens <lachlan@ratjens.com.au>
2024-04-04 12:00:17 +1100

    Fix variable names

--- backend/db-setup.py
+++ backend/db-setup.py
@@ -2,9 +2,9 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()  # Load environment variables from a .env file
-username = os.getenv('PG_USERNAME')
-password = os.getenv('PG_PASSWORD')
-password = os.getenv('PG_HOSTNAME')
-password = os.getenv('PG_PORT')
-password = os.getenv('PG_DB')
-password = os.getenv('PG_EXTERNAL_DB_URL')
+pg_username = os.getenv('PG_USERNAME')
+pg_password = os.getenv('PG_PASSWORD')
+pg_hostname = os.getenv('PG_HOSTNAME')
+pg_port = os.getenv('PG_PORT')
+pg_db = os.getenv('PG_DB')
+pg_external_db_url = os.getenv('PG_EXTERNAL_DB_URL')
